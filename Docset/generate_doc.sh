22687d8c7b830b2adb019f5c70ddc5d2202bfa46
Jeremie CUADRADO <jeremie_cuadrado@carrefour.com>
2019-09-21 22:01:21 +0200

    add $PATH_INDEX

--- Docset/generate_doc.sh
+++ Docset/generate_doc.sh
@@ -8,22 +8,26 @@ clear
 echo "
 ########################################
 #
-#   $(pwd)/$job_name -d my_docset_path -i redbeard28/docset:1.0 -m html
+#   $(pwd)/$job_name -d my_docset_path -i redbeard28/docset:1.0 -m html -p 'redbeard28/index.html'
 #
 #########################################
-option -d =>    /tmp/docs                                                   =>  destination path of your docset
-option -i =>    redbeard28/docset:1.0                                       => Image Name
-option -m =>    html or serve                                               => Generate html files or make server
+option -d =>    /tmp/docs                                 =>  destination path of your docset
+option -i =>    redbeard28/docset:1.0                     => Image Name
+option -m =>    html or serve                             => Generate html files or make server
+option -p =>    redbeard28/index.html                     => Tell where is your index.html page with full path from Documents
 "
 }
 
 #################################
 ### Recuperation des entrees
-while getopts d:m:h OPTION
+while getopts d:m:p:h OPTION
 do
   case "$OPTION" in
   d)
     DOCSET_PATH="$OPTARG"
+    ;;
+  p)
+    $PATH_INDEX="$OPTARG"
     ;;
   m)
     TYPE="$OPTARG"
@@ -48,7 +52,7 @@ generate_html() {
 
     mkdir redbeard28
     cp -rf docs/* redbeard28/
-    python /html2dash.py -n redbeard28 -d release -i docs_src/images/icon.png redbeard28 -p "redbeard28/index.html"
+    python /html2dash.py -n redbeard28 -d release -i docs_src/images/icon.png redbeard28 -p $PATH_INDEX
     sleep 5
     # Fix for icon.ico bug
     cp docs_src/images/favicon.ico release/redbeard28.docset/icon.ico
